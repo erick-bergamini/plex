16b26142c4ecf69aafea9f63957a7f515b84f7a4
Allan Garcia <allan.garcia@gmail.com>
2017-04-16 00:14:29 -0300

    Trailers are working now.

--- download-trailers.sh
+++ download-trailers.sh
@@ -1,33 +1,60 @@
 #!/bin/bash
-
-# download trailers from themoviedb website
-
+# Download trailers from themoviedb using API
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 FOLDER="$1"
 
+API_KEY="PUT_YOUR_THEMOVIEDB_API_KEY_HERE"
+LANG="pt-BR"
 M_NFO="movie.nfo"
-M_PATTERN="trailer"
-    
+M_TRAILER_KEY=""
+M_TRAILER_NAME=""
+M_TRAILER_YOUTUBE=0
+
 cd "${FOLDER}"
     
 if [ -f $M_NFO ]; then
-     
-    M_NUM=$(grep -c "$M_PATTERN" $M_NFO)
-        
+
+    M_ID=$(cat $M_NFO | grep "tmdb" | grep -oP "id='\K[^']*")
+
+    M_RESPONSE=$(curl -s -o - --request GET --url "https://api.themoviedb.org/3/movie/${M_ID}/videos?language=${LANG}&api_key=${API_KEY}" --data "{}")
+    
+    M_NUM=$(echo "$M_RESPONSE" | grep -c "Trailer")
+   
     if [[ $M_NUM -ge 1 ]]; then
             
-        IFS=$'\n'
-        for M_LINE in `grep "$M_PATTERN" $M_NFO`; do
-            echo $M_LINE
-            T_DESC=$(echo $M_LINE | grep -oP "name='\K[^']*")
-            echo "T_DESC = ${T_DESC}"
-            T_URL=$(echo $M_LINE | grep -oP ">\K[^<]*")
-            echo "T_URL = ${T_URL}"
-            youtube-dl -f 22 -o "${T_DESC}-trailer.%(ext)s" "${T_URL}"
-            echo "Just executed: youtube-dl -f 22 -o \"${T_DESC}-trailer.%(ext)s\" \"${T_URL}\""
+        IFS=$'},{'
+        for M_LINE in `echo "$M_RESPONSE" | grep -oP "\[\K[^\]]*"`; do
+            if [[ "$M_LINE" =~ "key" ]]; then
+              M_TRAILER_KEY=$(echo $M_LINE | grep -oP ":\"\K(\w+)")
+            fi
+            if [[ "$M_LINE" =~ "name" ]]; then
+              M_TRAILER_NAME=$(echo $M_LINE | grep -oP ":\"\K([\w -]+)")
+            fi
+            if [[ "$M_LINE" =~ "site" ]]; then
+              M_TRAILER_YOUTUBE=0
+              if [[ "$M_LINE" =~ "YouTube" ]]; then
+                M_TRAILER_YOUTUBE=1
+              fi
+            fi
+            if [[ "$M_LINE" =~ "type" ]]; then
+              if [[ "$M_LINE" =~ "Trailer" ]]; then
+                if [[ $M_TRAILER_YOUTUBE == 1 ]]; then
+                  echo "Found!"
+                  echo "Name: $M_TRAILER_NAME"
+                  echo "Key: $M_TRAILER_KEY"
+                  youtube-dl -f 22 -o "${M_TRAILER_NAME}-trailer.%(ext)s" "https://www.youtube.com/watch?v=${M_TRAILER_KEY}"
+                fi
+              fi
+            fi
         done
             
     fi
         
 fi
+
+# sanity check
+chown -R plex: "${FOLDER}"
+find "${FOLDER}" -type f -exec chmod 664 {} \;
+find "${FOLDER}" -type d -exec chmod 775 {} \;
+
